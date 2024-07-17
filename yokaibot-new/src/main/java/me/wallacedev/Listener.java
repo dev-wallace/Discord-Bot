f344af6eec94ad550a369d6ee85bb0d5c8f58b97
yokaithefato <wallacewwc2015wwk@gmail.com>
2024-07-17 13:04:37 -0300

    adding commands for all guilds (discord servers which bot is on)

--- yokaibot-new/src/main/java/me/wallacedev/Listener.java
+++ yokaibot-new/src/main/java/me/wallacedev/Listener.java
@@ -4,9 +4,9 @@ import org.jetbrains.annotations.NotNull;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
 import net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
